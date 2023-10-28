750abba9e489e4c3dbc16f2539b02e45543f3d9a
Daniel Arcos <arcosdaniel345@gmail.com>
2023-10-28 17:53:06 -0600

    first commit

---
+++ test-jenkins-docker-integration/src/main/java/system/test/jenkins/docker/integration/TestJenkinsDockerIntegration.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package system.test.jenkins.docker.integration;
+
+/**
+ *
+ * @author Daniel García Arcos
+ */
+public class TestJenkinsDockerIntegration {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
